bb3e4046a1854df7f2dd8c8a34cf235111da61f2
riffAt2013 <rifat.masud@northsouth.edu>
2022-03-11 01:22:17 +0600

    carousel image next/image-ified ;P

--- components/carousel.js
+++ components/carousel.js
@@ -2,12 +2,13 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation } from "swiper";
 import "swiper/css/navigation";
 import "swiper/css";
+import Image from "next/image";
 
 export default function Carousel() {
   return (
     <>
       {/* carousel container */}
-      <div className="sm:p-4 md:py-4 md:px-0 max-w-6xl mx-auto">
+      <div className="sm:p-4 md:py-4 md:px-0 max-w-7xl mx-auto">
         <Swiper
           modules={[Navigation]}
           spaceBetween={10}
@@ -19,12 +20,21 @@ export default function Carousel() {
           <SwiperSlide>
             <div className="w-full relative">
               <div className="w-full h-full bg-slate-500 top-0 left-0 absolute opacity-30"></div>
-              <div className="w-full h-full absolute -top-10 left-0 flex flex-col justify-center items-center z-50">
+              <div className="w-full h-full absolute  left-0 flex flex-col justify-center items-center z-50">
                 <h1 className="text-2xl md:text-5xl font-bold text-white">
                   Welcome to NSU Mars Ex!
                 </h1>
               </div>
-              <img src="/hero/carousel_3.jpg" alt="rover on site" />
+
+              <div className="">
+                <Image
+                  src="/hero/carousel_3.jpg"
+                  alt="rover portrait"
+                  layout="responsive"
+                  width={1024}
+                  height={600}
+                />
+              </div>
             </div>
           </SwiperSlide>
           <SwiperSlide>
@@ -35,7 +45,16 @@ export default function Carousel() {
                   Team with the rover
                 </h1>
               </div>
-              <img src="/hero/carousel_2.jpg" alt="team with the rover" />
+
+              <div className="">
+                <Image
+                  src="/hero/carousel_2.jpg"
+                  alt="team with the rover"
+                  layout="responsive"
+                  width={1024}
+                  height={600}
+                />
+              </div>
             </div>
           </SwiperSlide>
           <SwiperSlide>
@@ -46,7 +65,15 @@ export default function Carousel() {
                   Welcome to NSU Mars Ex!
                 </h1>
               </div>
-              <img src="/hero/carousel_1.jpg" alt="rover portrait in campus" />
+              <div className="">
+                <Image
+                  src="/hero/carousel_1.jpg"
+                  alt="rover portrait"
+                  layout="responsive"
+                  width={1024}
+                  height={600}
+                />
+              </div>
             </div>
           </SwiperSlide>
         </Swiper>
