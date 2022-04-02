376e5adb932e6db36333352a3eb5ac2e19497752
riffAt2013 <rifat.masud@northsouth.edu>
2022-04-02 19:50:37 +0600

    propified some contents, removed image based social links

--- components/card.js
+++ components/card.js
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Card(props) {
   const teamHover =
-    " hover:border-4 hover:border-solid hover:border-zinc-500 hover:rounded-md";
+    " hover:border-4 hover:border-solid hover:border-zinc-500 hover:rounded-xl";
   return (
     <>
       <div className="p-4 lg:w-1/3 md:w-1/2">
@@ -20,30 +20,51 @@ export default function Card(props) {
               {props.name}
             </h2>
             <h3 className="text-gray-500 mb-3">{props.title}</h3>
-            <p className="mb-4">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit modi
-              fuga expedita. Minima beatae nisi, eveniet quod voluptatem porro
-              accusantium.
-            </p>
+            <p className="mb-4">{props.bio}</p>
 
-            <span className="inline-flex space-x-5">
-              <div className="w-7 h-7 relative">
-                <a href="#">
-                  <Image src="/facebook.png" alt="facebook" layout="fill" />
-                </a>
-              </div>
+            <span className="inline-flex space-x-4">
+              {/* facebook url */}
+              <a href={props.fblink} className="hover:scale-125">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  style={{ msFilter: "" }}
+                  fill="rgba(0, 0, 0, 1)"
+                >
+                  <path d="M12.001 2.002c-5.522 0-9.999 4.477-9.999 9.999 0 4.99 3.656 9.126 8.437 9.879v-6.988h-2.54v-2.891h2.54V9.798c0-2.508 1.493-3.891 3.776-3.891 1.094 0 2.24.195 2.24.195v2.459h-1.264c-1.24 0-1.628.772-1.628 1.563v1.875h2.771l-.443 2.891h-2.328v6.988C18.344 21.129 22 16.992 22 12.001c0-5.522-4.477-9.999-9.999-9.999z"></path>
+                </svg>
+              </a>
 
-              <div className="w-7 h-7 relative">
-                <a href="#">
-                  <Image src="/github.png" alt="github" layout="fill" />
-                </a>
-              </div>
+              {/* github url */}
+              <a href={props.gitlink} className="hover:scale-125">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  style={{ msFilter: "" }}
+                  fill="rgba(0, 0, 0, 1)"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M12.026 2c-5.509 0-9.974 4.465-9.974 9.974 0 4.406 2.857 8.145 6.821 9.465.499.09.679-.217.679-.481 0-.237-.008-.865-.011-1.696-2.775.602-3.361-1.338-3.361-1.338-.452-1.152-1.107-1.459-1.107-1.459-.905-.619.069-.605.069-.605 1.002.07 1.527 1.028 1.527 1.028.89 1.524 2.336 1.084 2.902.829.091-.645.351-1.085.635-1.334-2.214-.251-4.542-1.107-4.542-4.93 0-1.087.389-1.979 1.024-2.675-.101-.253-.446-1.268.099-2.64 0 0 .837-.269 2.742 1.021a9.582 9.582 0 012.496-.336 9.554 9.554 0 012.496.336c1.906-1.291 2.742-1.021 2.742-1.021.545 1.372.203 2.387.099 2.64.64.696 1.024 1.587 1.024 2.675 0 3.833-2.33 4.675-4.552 4.922.355.308.675.916.675 1.846 0 1.334-.012 2.41-.012 2.737 0 .267.178.577.687.479C19.146 20.115 22 16.379 22 11.974 22 6.465 17.535 2 12.026 2z"
+                    clipRule="evenodd"
+                  ></path>
+                </svg>
+              </a>
 
-              <div className="w-7 h-7 relative">
-                <a href="#">
-                  <Image src="/mail.png" alt="email" layout="fill" />
-                </a>
-              </div>
+              {/* mail url */}
+              <a href={props.maillink} className="hover:scale-125">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  style={{ msFilter: "" }}
+                  fill="rgba(0, 0, 0, 1)"
+                >
+                  <path d="M20 4H4a2 2 0 00-2 2v12a2 2 0 002 2h16a2 2 0 002-2V6a2 2 0 00-2-2zm0 4.7l-8 5.334L4 8.7V6.297l8 5.333 8-5.333V8.7z"></path>
+                </svg>
+              </a>
             </span>
           </div>
         </div>
