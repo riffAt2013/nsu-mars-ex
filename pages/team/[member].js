e82beb7d7c896c10e8c7a2049a1b8601c2a0ecd3
riffAt2013 <rifat.masud@northsouth.edu>
2022-04-06 14:16:47 +0600

    !!add data fetching with api + fireAdmin

--- pages/team/[member].js
+++ pages/team/[member].js
@@ -1,10 +1,28 @@
 import { useRouter } from "next/router";
 
-export default function Member() {
+export async function getServerSideProps(context) {
+  return {
+    props: context.query, // will be passed to the page component as props
+  };
+}
+
+export default function Member(props) {
+  const imageData = {
+    afnan: "/teammem/afnan-min.jpg",
+    ridoy: "/teammem/shoumik-min.jpg",
+    monsur: "/teammem/monsur-min.jpg",
+    riad: "/teammem/riad-min.jpg",
+    zia: "/teammem/zia-min.jpg",
+    rifat: "/teammem/rifat-min.jpg",
+    nirob: "/teammem/nirob-min.jpg",
+    indra: "/teammem/indra-min.jpg",
+  };
+
   const router = useRouter();
+  const imageSrc = imageData[router.query.member];
   return (
     <>
-      <p>Hello World from the pageName: {router.query.member} </p>
+      <h1> Hello, {props.member}! </h1>
     </>
   );
 }
