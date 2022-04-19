9d54acc7f254fd72b5b1b2480848457ebce60822
riffAt2013 <rifat.masud@northsouth.edu>
2022-04-20 02:09:53 +0600

    added some infomational contents + added youtube vidlink

--- pages/index.js
+++ pages/index.js
@@ -16,14 +16,17 @@ export default function Home() {
       </div>
 
       {/* about */}
-      <div
+      <p
         id="about"
-        className="mt-10 py-3 bg-amber-800 rounded-xl w-3/4 px-6 mx-auto"
+        className="mt-10 text-xl font-extrabold md:text-4xl underline text-center"
       >
+        WHO ARE WE
+      </p>
+      <div className="mt-5 py-3 bg-amber-800 rounded-xl w-3/4 px-6 mx-auto">
         <p className="text-center text-white text-base md:text-xl">
           We are a group of students who are passionate about Robotics and
           Artificial Intellingence and share a common love for building space
-          rover. Founded in 2019, NSU Mars-Ex is the leading rover building team
+          rover. Founded in 2018, NSU Mars-Ex is the leading rover building team
           of North South University.
         </p>
       </div>
@@ -34,15 +37,27 @@ export default function Home() {
 
       {/* contact us section */}
       <p className="text-center text-xl pb-3">Find us here</p>
-      <div className="pb-10 flex flex-row justify-center">
+
+      <div className="py-24 flex flex-col items-center space-y-10 lg:flex-row lg:justify-around lg:space-x-20 lg:space-y-0 bg-amber-800">
+        {/* maps embed */}
         <iframe
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1063.7716656819332!2d90.4263015280892!3d23.814817995646635!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x2af70205eb36eb58!2zMjPCsDQ4JzU0LjMiTiA5MMKwMjUnMzQuNSJF!5e0!3m2!1sen!2sbd!4v1649186792356!5m2!1sen!2sbd"
-          width="500"
-          height="450"
-          style={{ border: 0 }}
-          // allowfullscreen=""
-          loading="lazy"
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d14702.04597150394!2d90.42323171168844!3d23.814228215652506!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755c64c125b6dc9%3A0x8ab3bb2e7c784ebc!2sNSU%20SAC!5e0!3m2!1sen!2sbd!4v1650395147523!5m2!1sen!2sbd"
+          width="400"
+          height="315"
+          style={{ borderRadius: "6px" }}
+          loading="eager"
           referrerpolicy="no-referrer-when-downgrade"
+        ></iframe>
+
+        {/* youtube video link */}
+        <iframe
+          width="460"
+          height="315"
+          src="https://www.youtube.com/embed/kzSMaFxnO3I?start=10"
+          title="mars-ex intro video"
+          style={{ borderRadius: "6px" }}
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
         ></iframe>
       </div>
     </div>
